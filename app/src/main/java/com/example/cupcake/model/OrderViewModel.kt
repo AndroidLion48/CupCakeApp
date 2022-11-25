665176982ec41dda21fde618c5e65f0bfb4c517f
AndroidLion48 <>
2022-11-24 16:33:31 -0800

    Giving fragments functionality

--- app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
+++ app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -3,6 +3,8 @@ package com.example.cupcake.model
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.text.SimpleDateFormat
+import java.util.*
 
 /**
  * Created by Clarence E Moore on 2022-11-24.
@@ -13,16 +15,19 @@ import androidx.lifecycle.ViewModel
  */
 class OrderViewModel: ViewModel() {
 
-    private val _quantity = MutableLiveData<Int>(0)
+    val dateOptions = getPickupOptions()
+
+    private val _quantity = MutableLiveData<Int>()
     val quantity: LiveData<Int> = _quantity
 
-    private val _flavor = MutableLiveData<String>("")
+    private val _flavor = MutableLiveData<String>()
     val flavor: LiveData<String> = _flavor
 
-    private val _date = MutableLiveData<String>("")
+    private val _date = MutableLiveData<String>()
     val date: LiveData<String> = _date
 
-    private val _price= MutableLiveData<Double>(0.0)
+    private val _price= MutableLiveData<Double>()
+    val price: LiveData<Double> = _price
 
     fun setQuantity(numberCupcakes: Int) {
         _quantity.value = numberCupcakes
@@ -39,4 +44,29 @@ class OrderViewModel: ViewModel() {
     fun setDate(pickupDate: String) {
         _date.value = pickupDate
     }
+
+    private fun getPickupOptions(): MutableList<String> {
+        val options = mutableListOf<String>()
+        val formatter = SimpleDateFormat("E MMM d", Locale.getDefault())
+        val calendar = Calendar.getInstance()
+
+        // Create a list of dates starting with the current date and the following 3 dates
+        repeat(4) {
+            options.add(formatter.format(calendar.time))
+            calendar.add(Calendar.DATE, 1)
+        }
+        return options
+    }
+
+    fun resetOrder(){
+        _quantity.value = 0
+        _date.value = dateOptions[0]
+        _flavor.value = ""
+        _price.value = 0.0
+    }
+
+    init {
+        resetOrder()
+    }
+
 }
